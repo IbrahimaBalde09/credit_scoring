cc02040118cb86cbcf6b97a9a034a5c1643b3fe8
Ibrahima Balde <baldei.ibrahima.bi23@gmail.com>
2026-03-18 12:03:58 +0100

    Fix settings and Procfile for Render

--- config/settings.py
+++ config/settings.py
@@ -3,9 +3,9 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "django-insecure-change-this-later"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-dev")
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     "localhost",
